3c5988c664a2cb56adf5ef72b6e930f483935e2c
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-05-03 15:56:16 +0200

    add jwt

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const express = require("express");
+const router = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
-router.post('/', registerUser)
-router.post('/login', loginUser)
 
-module.exports = router
+router.post("/", registerUser);
+router.post("/login", loginUser);
+router.get("/me", protect, getMe); // add "protect" as a secont argument
+
+module.exports = router;
